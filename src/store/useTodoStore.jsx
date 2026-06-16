2340e7d106bd45773a228206a1734ae6f54af1a2
Felicia Löfström <Flizan_Linnea@hotmail.com>
2026-06-16 11:32:49 +0200

    fix errors in useTodoStore

--- src/store/useTodoStore.jsx
+++ src/store/useTodoStore.jsx
@@ -6,7 +6,7 @@ export const useTodoStore = create((set) => ({
   // Add new todo
   addTodo: (text) => {
     set((state) => ({
-      todos: [...state.todos, { id: Date.now(, text, completed: false }]
+      todos: [...state.todos, { id: Date.now0(), text, completed: false }]
     }))
   },
 
@@ -21,7 +21,7 @@ export const useTodoStore = create((set) => ({
   completedTodo: (id) => {
     set((state) => ({
       todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed }
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
       )
     }))
   },
