47e83a379def2be867b3a2fa25bc42be6f8182e8
Felicia Löfström <Flizan_Linnea@hotmail.com>
2026-06-17 00:09:19 +0200

    final layout, and functions set

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { TodoList } from "./components/TodoList"
+import { use } from "react"
 
 export const App = () => {
+  const [isLightMode, setIsLightMode] = useState(false)
+
+  useEffect(() => {
+    if (isLightMode) {
+      document.body.classList.add('light-theme')
+    } else {
+      document.body.classList.remove('light-theme')
+    }
+  }, [isLightMode])
+
   return (
-    <div className="App">
+    <main>
+      {/* Dark Mode Light Mode switch btn */}
+      <button
+        className="theme-toggle-btn"
+        onClick={() => setIsLightMode(!isLightMode)}
+      >
+        {isLightMode ? '🌙 Dark Mode' : '☀️ Light Mode'}
+      </button>
+
       <header className="app-header">
         <h1>🎯Gör-Det-Listan</h1>
         <p>"Börja där du står. Använd det du har."</p>
       </header>
 
-      <main>
-        <TodoList />
-      </main>
+      <TodoList />
 
       <footer className="app-footer">
         <p>Built with React & Zustand • Made by Felicia</p>
       </footer>
-    </div>
+    </main >
   )
 }
+
+export default App
