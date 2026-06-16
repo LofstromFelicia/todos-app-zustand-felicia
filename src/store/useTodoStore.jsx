10e62ce990086151642758b1a52952d11aa24e6e
Felicia Löfström <Flizan_Linnea@hotmail.com>
2026-06-16 11:19:00 +0200

    create: useTodoStore.jsx

---
+++ src/store/useTodoStore.jsx
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+
+export const useTodoStore = create((set) => ({
+  todos: [],
+
+  // Add new todo
+  addTodo: (text) => {
+    set((state) => ({
+      todos: [...state.todos, { id: Date.now(, text, completed: false }]
+    }))
+  },
+
+  // remove todo
+  deleteTodo: (id) => {
+    set((state) => ({
+      todos: state.todos.filter((todo) => todo.id !== id)
+    }))
+  },
+
+  // check off / restore todo 
+  completedTodo: (id) => {
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed }
+      )
+    }))
+  },
+}))
